73f0fc5de2df85be95a4e896faea8dcbe329456f
Chris Nelson <superchrisnelson@gmail.com>
2023-08-17 17:38:20 -0400

    change language send custom event

--- assets/js/hello-world.ts
+++ assets/js/hello-world.ts
@@ -7,6 +7,17 @@ export class HelloWorldElement extends LitElement {
   greeting = '';
 
   render() {
-    return html`<h1>${this.greeting} <slot></slot>!</h1>`;
+    return html`
+      <h1>${this.greeting} <slot></slot>!</h1>
+      <select @change=${this.changeLanguage}>
+        <option>English</option>
+        <option>French</option>
+        <option>German</option>
+      </select>
+    `;
+  }
+
+  changeLanguage(event) {
+    this.dispatchEvent(new CustomEvent('change-language', { detail: {language: event.target.value} }));
   }
 }
