42e85a9f76547e9cd2db3768e8ddd9c7cad5fe62
Chris Nelson <superchrisnelson@gmail.com>
2023-09-05 15:22:51 -0400

    derp

--- assets/js/hello-world.ts
+++ assets/js/hello-world.ts
@@ -12,4 +12,21 @@ import { liveState } from "phx-live-state";
   }
 })
 export class HelloWorldElement extends LitElement {
+  @property({type: String})
+  greeting = '';
+
+  render() {
+    return html`
+      <h1>${this.greeting} <slot></slot>!</h1>
+      <select @change=${this.changeLanguage}>
+        <option>English</option>
+        <option>French</option>
+        <option>German</option>
+      </select>
+    `;
+  }
+
+  changeLanguage(event) {
+    this.dispatchEvent(new CustomEvent('change-language', { detail: {language: event.target.value} }));
+  }
 }
